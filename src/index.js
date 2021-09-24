bbdb6d87eb67f285a7a495a6172aa0d7a9736917
mrandolph83 <mrandolph83@gmail.com>
2021-09-24 01:03:52 -0400

    Modularized createStore Redux

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import store from './store.js'
+
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk'
 
-const users = () => []
-const reducer = combineReducers({
-  users
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
+
   
     <Provider store={ store }><App /></Provider>,
   
