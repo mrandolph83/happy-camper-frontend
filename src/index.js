74739963af3130d32f36e0993a44ce54e0eb303f
mrandolph83 <mrandolph83@gmail.com>
2021-09-23 23:31:26 -0400

    Reinstalled react-dedux, redux, thunk, middleware, and set up Redux elements to createStore

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+
+const users = () => []
+const reducer = combineReducers({
+  users
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <Provider store={ store }><App /></Provider>,
+  
   document.getElementById('root')
 );
 
