0457a8981fb6e45879805c2a36a26b130f624105
mrandolph83 <mrandolph83@gmail.com>
2021-09-24 11:46:20 -0400

    Moved Login.js into components folder

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <form onSubmit={undefined}> 
+            <input value={undefined} name="username" type="text" onChange={undefined} />
+            <input value={undefined} name="password" type="text" onChange={undefined} />
+            <input type="submit" value="Log In"/>
+        </form>
+    )
+}
+
+export default Login
