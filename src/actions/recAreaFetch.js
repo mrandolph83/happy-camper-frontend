d708bd21581c4b235ea178fecc70c2cbef41cff0
mrandolph83 <mrandolph83@gmail.com>
2021-09-28 01:11:40 -0400

    Scaffolded out API fetch request for recreation.gov

---
+++ src/actions/recAreaFetch.js
@@ -0,0 +1,75 @@
+
+//synchronous action creators
+// export const setCurrentUser = user => {
+//     return {
+//         type: "SET_CURRENT_USER",
+//         user
+//     }
+// }
+
+//async action creators
+
+// Search API for indexded Results
+
+export const recAreaQuery = (credentials) => {
+    const keyword = "Yosemite"
+    const stateCodes = "CA, OH, "
+    const activity = "11, 2, "
+    return  dispatch => {
+
+        return fetch(`https://ridb.recreation.gov/api/v1/recareas?query=${keyword}&limit=50&offset=0&full=true&state=${stateCodes}&activity=${activity}&sort=Name`, {
+            // credentials: "include", 
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "apikey": "c7e1bfea-aea3-436f-a007-619276d7bc13"
+                // Make into .env for security 
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        .then(r => r.json())
+        .then(response => {
+           console.log(response) 
+            
+        })
+    }
+}
+
+// export const clearIndex = () => {
+//     return (dispatch) => {
+//         dispatch(clearCurrentUser())
+//         return fetch("http://127.0.0.1:3000/api/v1/logout", {
+//             credentials: "include",
+//             method: "DELETE"
+//         })
+//     } 
+// }
+
+
+// export const getCurrentUser = () => {
+   
+//     return  dispatch => {
+
+//         return fetch("http://127.0.0.1:3000/api/v1/get_current_user", {
+//             credentials: "include",  
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//         })
+
+//         .then(r => r.json())
+//         .then(response => {
+//             if (response.error) {
+//                 alert(response.error)
+//             }
+//             else {
+//                 dispatch(setCurrentUser(response.data))
+//             }
+//         })
+//         .catch(console.log)
+//     }
+
+    
+// }
