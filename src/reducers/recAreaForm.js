0dd70b792f74d590f22f7a335984d34909299ce6
mrandolph83 <mrandolph83@gmail.com>
2021-09-28 12:00:43 -0400

    Add rec area form action and reducer

--- src/reducers/recAreaForm.js
+++ src/reducers/recAreaForm.js
@@ -12,10 +12,8 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case "UPDATE_LOGIN_FORM":
+        case "UPDATE_REC_FORM":
             return action.formData
-        case "RESET_LOGIN_FORM":
-            return initialState
         default:
             return state
     }
