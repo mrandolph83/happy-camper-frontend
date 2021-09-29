c249cc21884559e15ef7829cc6430b0cacc588b8
mrandolph83 <mrandolph83@gmail.com>
2021-09-29 01:22:58 -0400

    Successful communication to RecAreaQuery with Rec Area form params

--- src/components/RecAreaSearch.js
+++ src/components/RecAreaSearch.js
@@ -3,38 +3,38 @@ import { connect } from 'react-redux'
 import { updateRecAreaForm } from "../actions/recAreaForm.js"
 import { recAreaUserSearch } from "../actions/recAreaQuery.js"
 
-const RecAreaSearch = ({recAreaForm, updateRecAreaForm, recAreaUserSearch }) => {
+const RecAreaSearch = ({recAreaFormData, updateRecAreaForm}) => {
 
     const handleInputChange = event => {
         const { name, value } = event.target
         const updatedFormInfo = {
-            ...recAreaForm,
+            ...recAreaFormData,
             [name]: value 
         }
         updateRecAreaForm(updatedFormInfo)
     }
 
     const handleSubmit = event => {
-        event.preventDefault()
-        recAreaUserSearch(recAreaForm)
+        event.preventDefault()  
+        recAreaUserSearch(recAreaFormData)
     }
 
 // Set hidden values on form for the activities and their numbers (ie "1, ")
 // You can set a data type of "number" that has parameters, use this for review later
 
-    return (
-        <form onSubmit={handleSubmit}> 
-            
-            <input placeholder="activity1" value={recAreaForm.activity1} name="activity1" type="text" onChange={handleInputChange} />
-            <input placeholder="activity2" value={recAreaForm.activity2} name="activity2" type="text" onChange={handleInputChange} />
-            <input placeholder="activity3" value={recAreaForm.activity3} name="activity3" type="text" onChange={handleInputChange} />
-            <input placeholder="activity4" value={recAreaForm.activity4} name="activity4" type="text" onChange={handleInputChange} />
-            <input placeholder="activity5" value={recAreaForm.activity5} name="activity5" type="text" onChange={handleInputChange} />
-            <input placeholder="stateCode1" value={recAreaForm.stateCode1} name="stateCode1" type="text" onChange={handleInputChange} />
-            <input placeholder="stateCode2" value={recAreaForm.stateCode1} name="stateCode2" type="text" onChange={handleInputChange} />
-            <input placeholder="stateCode3" value={recAreaForm.stateCode1} name="stateCode3" type="text" onChange={handleInputChange} />
-            <input type="submit" value="Search Rec Areas"/>
-        </form>
+return (
+    <form onSubmit={handleSubmit}> 
+        
+        <input placeholder="activity1" value={recAreaFormData.activity1} name="activity1" type="text" onChange={handleInputChange} />
+        <input placeholder="activity2" value={recAreaFormData.activity2} name="activity2" type="text" onChange={handleInputChange} />
+        <input placeholder="activity3" value={recAreaFormData.activity3} name="activity3" type="text" onChange={handleInputChange} />
+        <input placeholder="activity4" value={recAreaFormData.activity4} name="activity4" type="text" onChange={handleInputChange} />
+        <input placeholder="activity5" value={recAreaFormData.activity5} name="activity5" type="text" onChange={handleInputChange} />
+        <input placeholder="stateCode1" value={recAreaFormData.stateCode1} name="stateCode1" type="text" onChange={handleInputChange} />
+        <input placeholder="stateCode2" value={recAreaFormData.stateCode2} name="stateCode2" type="text" onChange={handleInputChange} />
+        <input placeholder="stateCode3" value={recAreaFormData.stateCode3} name="stateCode3" type="text" onChange={handleInputChange} />
+        <input type="submit" value="Search Rec Areas"/>
+    </form>
     )
 }
 
@@ -46,7 +46,7 @@ const RecAreaSearch = ({recAreaForm, updateRecAreaForm, recAreaUserSearch }) => 
 
 const mapStateToProps = state => {
     return {
-       recAreaForm: state.recAreaForm
+       recAreaFormData: state.recAreaForm
     }
 }
 
