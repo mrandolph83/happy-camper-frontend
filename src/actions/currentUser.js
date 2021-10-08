d3d8de0eaa972b18d0a706f44011bbe21a9e7162
mrandolph83 <mrandolph83@gmail.com>
2021-10-08 13:46:43 -0400

    Added key of userInfo to credentials on the signup async request. USer signup works properly on frontend and backend now

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,13 +1,15 @@
 import { resetLoginForm } from "./loginForm.js"
+import { resetSignupForm } from "./signupForm.js"
+
 import { getMyTrips } from "./userFavorites.js"
 
 //synchronous action creators
 export const setCurrentUser = user => {
     return {
-        type: "SET_CURRENT_USER",
-        user
+      type: "SET_CURRENT_USER",
+      user
     }
-}
+  }
 
 export const clearCurrentUser = () => {
     return {
@@ -55,6 +57,40 @@ export const logout = () => {
         })
     } 
 }
+
+export const signup = (credentials) => {
+    console.log("credentials are", credentials)
+    return  dispatch => {
+// credentials must be added to a key (user: credentials) before being sent to backend
+// so that ActionController can read it as a new object to be created with these attributes
+        const userInfo = {
+            user: credentials
+        }
+        return fetch("http://127.0.0.1:3000/api/v1/signup", {
+            credentials: "include", 
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userInfo)
+        })
+
+        .then(r => r.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error)
+            }
+            else {
+                
+                dispatch(setCurrentUser(response.data))
+                // dispatch(getUserFavorites())
+                dispatch(resetSignupForm())
+            }
+        })
+        .catch(console.log)
+    }
+}
+
 
 
 export const getCurrentUser = () => {
