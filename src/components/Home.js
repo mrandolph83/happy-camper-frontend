59447752747d5f9f5e9a59d5da3984a220cb262d
mrandolph83 <mrandolph83@gmail.com>
2021-10-08 14:27:53 -0400

    Created Home component

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from './componenets/Login.js'
+import Logout from './componenets/Logout.js'
+
+const Home = ({}) => (
+    <div>Welcome, please log in or sign up</div>
+);
+
+export default Home
