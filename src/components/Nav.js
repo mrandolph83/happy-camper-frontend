3a30e68983a40b7dedcb5a28478470209d17ec51
mrandolph83 <mrandolph83@gmail.com>
2021-10-11 15:29:18 -0400

    Debug Logout ternary on NavBar component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,7 +12,7 @@ const NavBar = ({ currentUser, loggedIn }) => {
              <NavLink to="/rec_area_search">Discover</NavLink>
              <NavLink to="/reviews/new">Share</NavLink>
              <NavLink to="/explore">Explore</NavLink>
-             { loggedIn ? <Logout/> : null}
+             { loggedIn ? <Logout/> : null }
 
         </div>
     )
@@ -21,7 +21,7 @@ const NavBar = ({ currentUser, loggedIn }) => {
 const mapStateToProps = ({ currentUser }) => {
     return {
         currentUser,
-        logged_in: !!currentUser
+        loggedIn: !!currentUser
 
     }
 }
