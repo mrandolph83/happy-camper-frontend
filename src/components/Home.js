ebe74044f6df1e89aa364c90e6162d73098c2517
mrandolph83 <mrandolph83@gmail.com>
2021-10-11 14:27:26 -0400

    Setup home/index page for app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Login from './componenets/Login.js'
-import Logout from './componenets/Logout.js'
+// import Login from './componenets/Login.js'
+// import Signup from './componenets/Signup.js'
+// Allows access to link methods for log in & signup
+import { Link } from 'react-router-dom'
 
 const Home = ({}) => (
-    <div>Welcome, please log in or sign up</div>
+    <div>
+        <span>Welcome, please <Link to="/login">log In</Link> or <Link to="/signup"> sign Up </Link> </span>
+  
+        </div>
 );
 
 export default Home
