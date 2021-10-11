7b0b4c946f19d1579ca5e530d1ba05989547766b
mrandolph83 <mrandolph83@gmail.com>
2021-10-11 14:33:03 -0400

    Scaffold form to submit a new review, git push

--- src/components/NewReview.js
+++ src/components/NewReview.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { updateNewReviewForm } from '../actions/newReview.js'
+import { connect } from 'react-redux'
 
 // Should be receiving props of current_user id, location_id
 
-const NewReviewForm = () => {
+
+// 
+
+const NewReviewForm = ({user_id, rec_area_id, description, picture, nature_review, amenities_review, actitivities_review, family_review}) => {
    const handleChange = event => {
       const { name, value } = event.target
       updateNewReviewForm(name, value)
    }
 
+const handleSubmit = event => event.preventDefault(); {
+
+}
+
 return ( 
-<form>
+<form onSubmit={handleSubmit}>
 
 <input
 name="user_id"
@@ -60,10 +68,18 @@ onChange={handleChange}
 value={""}
 />
 
+<input type="submit" value="Create Review"/>
+
 </form>
 )};
 
-export default NewReviewForm
+const mapStateToProps = state => {
+   const {user_id, rec_area_id, description, picture, nature_review, amenities_review, actitivities_review, family_review} = state.newReview
+   return {user_id, rec_area_id, description, picture, nature_review, amenities_review, actitivities_review, family_review}
+}
+
+
+export default connect(mapStateToProps, {updateNewReviewForm})(NewReviewForm)
 
 
 
