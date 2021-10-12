3f9ab485a54f4915f1ae3b72b13c5230e4cfe787
mrandolph83 <mrandolph83@gmail.com>
2021-10-12 02:29:58 -0400

    Functional review form, need to check currentUser user_id functionality

--- src/components/NewReview.js
+++ src/components/NewReview.js
@@ -1,71 +1,86 @@
 import React from 'react';
 import { updateNewReviewForm } from '../actions/newReview.js'
+import { createReview } from '../actions/newReview.js'
 import { connect } from 'react-redux'
 
 // Should be receiving props of current_user id, location_id
 
 
 // 
+// Destructuring, look at making form video again. Took a still with notes on it 
+const NewReviewForm = ({user_id, rec_area_id, description, picture, nature_review, amenities_review, activities_review, family_review, history, updateNewReviewForm, createReview}) => {
 
-const NewReviewForm = ({user_id, rec_area_id, description, picture, nature_review, amenities_review, actitivities_review, family_review}) => {
+   // const {user_id, rec_area_id, description, date, picture, nature_review, amenities_review, activities_review, family_review} = formData
+
    const handleChange = event => {
       const { name, value } = event.target
       updateNewReviewForm(name, value)
    }
 
-const handleSubmit = event => event.preventDefault(); {
-
+const handleSubmit = event => {
+   event.preventDefault();
+   createReview(user_id, rec_area_id, description, picture, nature_review, amenities_review, activities_review, family_review)
 }
+
+
 
 return ( 
 <form onSubmit={handleSubmit}>
 
-<input
+{/* <input
+placeholder="user_id"
 name="user_id"
 onChange={handleChange}
-value={""}
-/>
+value={user_id}
+/> */}
 
 <input
+placeholder="rec_area_id"
 name="rec_area_id"
 onChange={handleChange}
-value={""}
+value={rec_area_id}
 />
 
 <input
+placeholder="description"
 name="description"
 onChange={handleChange}
-value={""}
+value={description}
 />
 
 <input
+placeholder="picture"
 name="picture"
 onChange={handleChange}
-value={""}
+value={picture}
 />
 
 <input
+placeholder="nature_review"
 name="nature_review"
 onChange={handleChange}
-value={""}
+value={nature_review}
 />
 
 <input
+placeholder="amenities_review"
 name="amenities_review"
 onChange={handleChange}
-value={""}
+value={amenities_review}
 />
 
 <input
+placeholder="activities_review"
 name="activities_review"
 onChange={handleChange}
-value={""}
+value={activities_review}
 />
 
 <input
+
 name="family_review"
 onChange={handleChange}
-value={""}
+value={family_review}
 />
 
 <input type="submit" value="Create Review"/>
@@ -75,11 +90,11 @@ value={""}
 
 const mapStateToProps = state => {
    const {user_id, rec_area_id, description, picture, nature_review, amenities_review, actitivities_review, family_review} = state.newReview
-   return {user_id, rec_area_id, description, picture, nature_review, amenities_review, actitivities_review, family_review}
+   return {user_id, rec_area_id, description, picture, nature_review, amenities_review, actitivities_review, family_review, user_id: state.currentUser.id}
 }
 
 
-export default connect(mapStateToProps, {updateNewReviewForm})(NewReviewForm)
+export default connect(mapStateToProps, {updateNewReviewForm, createReview})(NewReviewForm)
 
 
 
