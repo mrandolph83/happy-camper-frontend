66f0e8958ebeada87beea725a102540c2f93dbf8
mrandolph83 <mrandolph83@gmail.com>
2021-10-12 02:27:08 -0400

    Add createReview action, not successfully creating on backend yet, updates needed

--- src/actions/newReview.js
+++ src/actions/newReview.js
@@ -4,3 +4,20 @@ export const updateNewReviewForm = (name, value) => {
         formData: { name, value }
     }
 }
+
+export const createReview = reviewData => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/reviews", { 
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+// Check what the credentials are
+
+        body: JSON.stringify(reviewData)
+        })
+        .then(r => r.json())
+        .then(console.log)
+    }
+}
