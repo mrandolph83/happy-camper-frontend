4394cf995f93be3a9c441d8e30ff96fb4cb2b306
mrandolph83 <mrandolph83@gmail.com>
2021-10-13 01:31:25 -0400

    Make dummy component which will be the user account page

---
+++ src/components/MyAccount.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MyAccount = ({}) => (
+    <div>
+        <h2>User Info</h2>
+        <p>User favorites on the left side in a list, 
+            User into they can edit in a link on the top R
+            User reviews listed belw that can be edited or deleted
+        </p>
+  
+        </div>
+);
+
+export default MyAccount
