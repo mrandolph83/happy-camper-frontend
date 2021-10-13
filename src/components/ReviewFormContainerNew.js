016c36f52e0cc692d8d96212a1b54a3aa13f309b
mrandolph83 <mrandolph83@gmail.com>
2021-10-13 12:01:51 -0400

    Set up new componenets edit and new Review forms, incomplete

---
+++ src/components/ReviewFormContainerNew.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createReview } from '../actions/recAreaForm';
+
+const NewReviewFormContainer = ({}) => (
+    <ReviewForm/>
+);
+export default NewReviewFormContainer;
