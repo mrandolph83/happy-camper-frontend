8eadb5fc28709c8c722c3417a83d7fd5d6ec303f
mrandolph83 <mrandolph83@gmail.com>
2021-10-13 11:57:11 -0400

    Update file names for new/edit Review form setup

--- src/actions/reviewForm.js
+++ src/actions/reviewForm.js
@@ -15,9 +15,21 @@ export const addReview = review => {
     }
 }
 
-export const resetReviewFormForm = (name, value) => {
+export const resetReviewForm = () => {
     return {
         type: "RESET_NEW_REVIEW_FORM", 
+    }
+}
+
+
+export const editReviewFormData = review => {
+    const reviewFormData = {
+        user_id: review.attributes.user_id
+        // Populate rest of data to set up for edit
+    }
+    return {
+        type: "EDIT_REVIEW_FORM_DATA",
+        reviewFormData
     }
 }
 
