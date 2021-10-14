4043fadcd3b441a18e896fd2a3ba805ef5847a9d
mrandolph83 <mrandolph83@gmail.com>
2021-10-14 16:28:36 -0400

    Set up ReviewForm to work as a partial, adding editForm ternary as props coming into this component

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { updateReviewForm } from '../actions/reviewForm.js'
-import { createReview } from '../actions/reviewForm.js'
 import { connect } from 'react-redux'
 
 // Should be receiving props of current_user id, location_id
@@ -8,7 +7,7 @@ import { connect } from 'react-redux'
 
 // 
 // Destructuring, look at making form video again. Took a still with notes on it 
-const ReviewForm = ({newReviewData, updateReviewForm, history, user_id, createReview}) => {
+const ReviewForm = ({newReviewData, history, updateReviewForm, user_id, review, handleSubmit, editMode}) => {
 
    // const {user_id, rec_area_id, description, date, picture, nature_review, amenities_review, activities_review, family_review} = formData
 
@@ -17,16 +16,16 @@ const ReviewForm = ({newReviewData, updateReviewForm, history, user_id, createRe
       updateReviewForm(name, value)
    }
 
-const handleSubmit = event => {
-   event.preventDefault();
-   createReview(
-      {...newReviewData, user_id}, history)
-}
+
 
 
 
 return ( 
-<form onSubmit={handleSubmit}>
+<form onSubmit={event => {
+  
+  
+   console.log("here we are!")
+   handleSubmit(event, newReviewData, user_id, history)}}>
 
 {/* <input
 placeholder="user_id"
@@ -94,7 +93,7 @@ onChange={handleChange}
 value={newReviewData.family_review}
 />
 
-<input type="submit" value="Create Review"/>
+<input type="submit" value={editMode ? "Update Review" : "Create Review"}/>
 
 </form>
 )};
@@ -108,7 +107,7 @@ const mapStateToProps = state => {
 }
 
 
-export default connect(mapStateToProps, { updateReviewForm, createReview })(ReviewForm)
+export default connect(mapStateToProps, { updateReviewForm })(ReviewForm)
 
 
 
