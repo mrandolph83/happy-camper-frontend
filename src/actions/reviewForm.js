f18dc4687a1ea9786f78497829803e31e6925c4d
mrandolph83 <mrandolph83@gmail.com>
2021-10-14 16:27:09 -0400

    Remove action creator functions that pertain to review(s) and moved them to actions/myReviews.js

--- src/actions/reviewForm.js
+++ src/actions/reviewForm.js
@@ -8,12 +8,7 @@ export const updateReviewForm = (name, value) => {
     }
 }
 
-export const addReview = review => {
-    return {
-        type: "ADD_REVIEW",
-        review
-    }
-}
+
 
 export const resetReviewForm = () => {
     return {
@@ -37,55 +32,3 @@ export const editReviewFormData = review => {
 // Check what the credentials are
 
 
-export const getMyReviews = () => {
-    return dispatch => {
-    return fetch("http://127.0.0.1:3000/api/v1/reviews",{
-        credentials: "include",
-        method: "GET",
-        headers: {
-        "Content-Type": "application/json"
-        },
-    }) 
-    .then(r => r.json())
-    .then(response => {
-        if (response.error) {
-            alert(response.error)
-    }
-        else {
-            console.log(response.data)
-        }
-    })
-    .catch(console.log)
-    
-    }
-}
-
-export const createReview = (reviewData, history) => {
-    console.log("reviewData is", reviewData)
-    return dispatch => {
-    return fetch("http://127.0.0.1:3000/api/v1/reviews", { 
-        credentials: "include",
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-            body: JSON.stringify(reviewData)
-        })
-
-        .then(r => r.json())
-        .then(review => {
-            if (review.error) {
-                alert(review.error)}
-                else {
-                    dispatch(addReview(review.data))
-                    history.push('/account')
-                    // dispatch(resetReviewFormForm())
-                    
-                    // redirects to user account to review the data
-                }
-            })
-            .catch(console.log)
-        }
-    }
-            
-            
