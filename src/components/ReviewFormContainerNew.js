67c1f2a2963aff0379001ea8787de602bfa17528
mrandolph83 <mrandolph83@gmail.com>
2021-10-14 16:31:16 -0400

    Update import of createReview action to actions/myReviews.js

--- src/components/ReviewFormContainerNew.js
+++ src/components/ReviewFormContainerNew.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import { createReview } from '../actions/recAreaForm';
+import { createReview } from '../actions/myReviews.js'
+import ReviewForm from './ReviewForm'
+import { connect } from 'react-redux'
 
-const NewReviewFormContainer = ({}) => (
-    <ReviewForm/>
-);
-export default NewReviewFormContainer;
+const NewReviewFormContainer = ({history, createReview}) => {
+
+
+    const handleSubmit = (event, newReviewData, user_id, history) => {
+        event.preventDefault()
+        createReview(
+           {...newReviewData, user_id}, history)
+     }
+
+     return <ReviewForm history={history} handleSubmit={handleSubmit} />
+};
+export default connect(null, { createReview })(NewReviewFormContainer);
