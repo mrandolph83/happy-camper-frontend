181f930b5ade560fe78bf6ee39fc441bf0257ad2
mrandolph83 <mrandolph83@gmail.com>
2021-10-14 16:29:58 -0400

    Make into class component to allow componentDidMount, which feeds the review to update as a prop into ReviewForm component

--- src/components/ReviewFormContainerEdit.js
+++ src/components/ReviewFormContainerEdit.js
@@ -1,1 +1,25 @@
 
+import React from 'react';
+import { updateReview, editReviewFormData } from '../actions/reviewForm.js'
+import ReviewForm from './ReviewForm'
+import { connect } from 'react-redux'
+
+class EditReviewFormContainer extends React.Component {
+componentDidMount(){
+this.props.review && this.props.editReviewFormData(this.props.review)
+
+}
+// Look into differences between making classes
+    handleSubmit = (event, newReviewData, user_id, history) => {
+    const { updateReview, review} = this.props
+    event.preventDefault()
+    updateReview(
+           {...newReviewData, user_id}, history)
+     }
+
+     render() {
+        const {history, handleSubmit} = this.props 
+        return <ReviewForm editMode history={history} handleSubmit={handleSubmit} />
+    }
+};
+export default connect(null, { updateReview, editReviewFormData })(EditReviewFormContainer);
