73a569a921db392145af931b0455f9b8328ef8aa
mrandolph83 <mrandolph83@gmail.com>
2021-10-15 01:38:32 -0400

    Update attributes to be read on ReviewCard

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 const ReviewCard = ({ review }) => {
   return (
     review ?
       <div>
-        <h3>{review.attributes.name}</h3>
-        <p>{review.attributes.start_date}</p>
-        <p>{review.attributes.end_date}</p>
-        <Link to={`/reviews/${review.id}/edit`}>Edit this review</Link>
+        <h3>{review.attributes.description}</h3>
+        <p>{review.attributes.picture}</p>
       </div> :
-      <p>This the the Review card with no review!</p>
+      <p> This is the trip card! </p>
+     
   )
 }
 
-export default ReviewCard
+export default ReviewCard;
