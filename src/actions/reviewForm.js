356f23d2de501f32590f58a67056b20134dc4f67
mrandolph83 <mrandolph83@gmail.com>
2021-10-15 23:34:33 -0400

    Tweak editReviewFormData action creator to include id and user_id after having issues with this integer data being read as strings

--- src/actions/reviewForm.js
+++ src/actions/reviewForm.js
@@ -19,13 +19,16 @@ export const resetReviewForm = () => {
 
 export const editReviewFormData = review => {
     const reviewFormData = {
-        user_id: review.attributes.user_id,
+        id: review.id,
         date: review.attributes.date,
+        description: review.attributes.description,
+        picture: review.attributes.picture,
         rec_area_id: review.attributes.rec_area_id,
         nature_review: review.attributes.nature_review,
         amenities_review: review.attributes.amenities_review,
         activities_review: review.attributes.activities_review,
-        family_review: review.attributes.family_review
+        family_review: review.attributes.family_review,
+        user_id: review.attributes.user_id
         
         // Populate rest of data to set up for edit
     }
