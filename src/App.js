431a6a0564b8cdcfa204a0e0938b9d7c51c33b74
mrandolph83 <mrandolph83@gmail.com>
2021-10-17 21:34:30 -0400

    Remove RecAreaSeach from App.js, move to DiscoverContainer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./components/Nav.js"
-import RecAreaSearch from "./components/RecAreaSearch.js"
-import MainContainer from "./components/MainContainer.js";
 import PathSelection from "./components/PathSelection.js"
+import DiscoverContainer from "./components/DiscoverContainer.js"
 import MyReviews from "./components/MyReviews.js"
 import ReviewCard from "./components/ReviewCard.js"
 import MyAccount from "./components/MyAccount.js"
-import Login from './components/Login.js'
 import Logout from './components/Logout.js'
 import Home from './components/Home.js'
 import Signup from './components/Signup'
 import NewReviewFormContainer from './components/ReviewFormContainerNew'
 import EditReviewFormContainer from './components/ReviewFormContainerEdit'
-import { Route, Switch, Link, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import { getCurrentUser } from "./actions/currentUser.js"
 import { getMyReviews } from "./actions/myReviews.js"
 import { connect } from "react-redux"
@@ -35,17 +33,18 @@ return (
 
   {/* location prop provided by withRouter */}
   <NavBar location={this.props.location}/> 
-  { loggedIn ? null : <Link to='/login'>Login</Link>}
  <Switch>
   {/* With render you can be more specific with what props you are sending,
   with component it is the whole component and all props in the ecosystem */}
- <Route exact path='/login' component={Login}/>
+ {/* <Route exact path='/login' component={Login}/> */}
+ <Route exact path='/' render={(props)=> loggedIn ? <PathSelection/> : <Home/>}/>
  <Route exact path='/logout' component={Logout}/>
  <Route exact path='/account' component={MyAccount}/>
  <Route exact path='/reviews/new' component={NewReviewFormContainer}/>
+ <Route exact path='/discover' component={DiscoverContainer}/>
  <Route exact path='/reviews' component={MyReviews}/>
  <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
- <Route exact path='/' render={(props)=> loggedIn ? <PathSelection/> : <Home/>}/>
+ 
  <Route exact path='/reviews/:id' render={props => {
     const review = reviews.find(review => review.id === props.match.params.id)
     return <ReviewCard review={review} {...props}/>  }}/>
