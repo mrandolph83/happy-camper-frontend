72094fe4d74861e395d9c7dfc0eda4528c1df459
mrandolph83 <mrandolph83@gmail.com>
2021-10-17 21:38:25 -0400

    Add links and update paths to reflect UI/pageflow prepro doc

--- src/components/PathSelection.js
+++ src/components/PathSelection.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const PathSelection = ({}) => (
     <div>
-        <h2>Please choose your path</h2>
-  
-        </div>
+        <h2>Choose Your Path</h2>
+
+    <span> 
+    <Link to="/discover"> DISCOVER </Link> 
+    <Link to="/reviews/new"> SHARE </Link>
+    <Link to="/explore"> EXPLORE</Link>
+    </span>
+    </div>
 );
 
 export default PathSelection
