6c9385d1fe7cfcf72a8772fbd5d8ebed4a4c3a74
mrandolph83 <mrandolph83@gmail.com>
2021-10-19 10:09:40 -0400

    Clean up comments

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,7 @@
 import React from 'react'
-// Needed because this is a React component
 import { connect } from 'react-redux'
-// Allows for mapStateToProps
 import { updateLoginForm } from "../actions/loginForm.js"
-// The action that takes in this data 
 import { login } from "../actions/currentUser.js"
-// Gives access to login const variable, which is the fetch to the
-// backend that authenticates the user and sets currentUser
 
 const Login = ({loginForm, updateLoginForm, login, history }) => {
 
@@ -27,17 +22,11 @@ const Login = ({loginForm, updateLoginForm, login, history }) => {
     return (
         <form onSubmit={handleSubmit}> 
             <input placeholder="username" value={loginForm.username} name="username" type="text" onChange={handleInputChange} />
-            <input placeholder="password" value={loginForm.password} name="password" type="text" onChange={handleInputChange} />
+            <input placeholder="password" value={loginForm.password} name="password" type="password" onChange={handleInputChange} />
             <input type="submit" value="Log In"/>
         </form>
     )
 }
-
-// Turns the state into props within this component. Sructured
-// like: {
-//     username: mrandolph83
-//     password: password
-// }
 
 const mapStateToProps = state => {
     return {
@@ -45,6 +34,4 @@ const mapStateToProps = state => {
     }
 }
 
-// Sends this data from the form through these actions, allowing the state to 
-// be updated
 export default connect(mapStateToProps, { updateLoginForm, login })(Login)
