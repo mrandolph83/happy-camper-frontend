72953210e808b165579fa959b6530035d6a2ad66
mrandolph83 <mrandolph83@gmail.com>
2021-10-21 19:29:49 -0400

    Add CSS/style to rec show page

---
+++ src/components/RecAreaCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+
+
+
+const RecAreaCard = (props) => {
+    
+    
+
+
+console.log(props)
+return ( 
+
+<>
+<div className="rec-show-result-box">
+
+<img className="rec-show-pic" src={props.recArea.attributes.images} width="350px" alt="Image not available" />
+<h2>{props.recArea.attributes.name}</h2>
+<h4>Activities:</h4>
+<p>{props.recArea.attributes.activities}</p>
+<p> <a href={`https://www.google.com/maps/dir//'${props.recArea.attributes.latitude},${props.recArea.attributes.longitude}'`} target="_blank">Get Directions</a></p>
+
+
+<p>{props.recArea.attributes.description}</p>
+
+
+
+
+</div>
+{/* end of box */}
+
+
+
+</>
+
+
+)}
+
+const mapStateToProps = state => {
+    return {
+        currentUser: state.currentUser,
+    }
+}
+
+export default connect(mapStateToProps)(RecAreaCard)
