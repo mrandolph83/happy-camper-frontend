5c47b56194bfbc2a6ff7fd7f151c9a4423824e05
mrandolph83 <mrandolph83@gmail.com>
2021-10-22 03:26:30 -0400

    CSS styling for Rec Area show page

--- src/components/RecAreaCard.js
+++ src/components/RecAreaCard.js
@@ -1,19 +1,13 @@
 import React from 'react'
-
+import { createUserFavorites } from '../actions/userFavorite'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 
-
-
 const RecAreaCard = (props) => {
     
-    
-
-
 console.log(props)
 return ( 
-
 <>
 <div className="rec-show-result-box">
 
@@ -21,28 +15,28 @@ return (
 <h1 className="rec-show-attributes">{props.recArea.attributes.name}</h1>
 <h4>Activities:</h4>
 <p>{props.recArea.attributes.activities}</p>
-<p> <a href={`https://www.google.com/maps/dir//'${props.recArea.attributes.latitude},${props.recArea.attributes.longitude}'`} target="_blank">Get Directions</a></p>
-
-
+<p> <a href={`https://www.google.com/maps/dir//'${props.recArea.attributes.latitude},${props.recArea.attributes.longitude}`} target="_blank">Get Directions</a></p>
 <p>{props.recArea.attributes.description}</p>
-
-
-
-
 </div>
-{/* end of box */}
 
+<form onSubmit={event => {
+    event.preventDefault()
+  const rec_area_id = props.recArea.id
+    const user_id = props.currentUser.id
+   createUserFavorites(rec_area_id, user_id)
+   }}>
 
-
+<button type="submit">Click to Review</button>
+</form>
 </>
-
 
 )}
 
 const mapStateToProps = state => {
     return {
         currentUser: state.currentUser,
+        userFavorite: state.userFavorite
     }
 }
 
-export default connect(mapStateToProps)(RecAreaCard)
+export default connect(mapStateToProps, { createUserFavorites })(RecAreaCard)
