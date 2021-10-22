99b038b569cdfa1659279a37e90ee31558ab8ff0
mrandolph83 <mrandolph83@gmail.com>
2021-10-22 03:29:33 -0400

    updated links

--- src/components/PathSelection.js
+++ src/components/PathSelection.js
@@ -6,7 +6,7 @@ const PathSelection = ({}) => (
         <h2>Choose Your Path</h2>
 
     <span> 
-    <Link to="/discover"> DISCOVER </Link> 
+    <Link to="/rec_area_search"> DISCOVER </Link> 
     <Link to="/reviews/new"> SHARE </Link>
     <Link to="/explore"> EXPLORE</Link>
     </span>
