92f96d85261b5b7be24563647351f5e30a084363
mrandolph83 <mrandolph83@gmail.com>
2021-10-25 14:06:30 -0400

    Path selection/links design and function

--- src/components/PathSelection.js
+++ src/components/PathSelection.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+
 const PathSelection = () => (
     <div>
-        <h2>Choose Your Path</h2>
+        <h2><img className="user-path-sign" height="100px" src="../images/path-selection-banner.png" /></h2>
+
 
     <span> 
-    <Link to="/rec_area_search"> DISCOVER </Link> 
-    <Link to="/reviews/new"> SHARE </Link>
+    <Link to="/rec_area_search"> <img className="path-sub-banners" height="300px" src="../images/discover-path.png" /> </Link> 
+    <Link to="/reviews/new"> <img className="path-sub-banners" height="300px" src="../images/my-reviews-path.png" />  </Link>
     {/* <Link to="/explore"> EXPLORE</Link> */}
     </span>
     </div>
 );
+
+
 
 export default PathSelection
