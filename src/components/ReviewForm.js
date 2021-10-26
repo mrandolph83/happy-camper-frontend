2400abb0f192a8912403046602adc54538bbf6e2
mrandolph83 <mrandolph83@gmail.com>
2021-10-25 23:39:44 -0400

    Updated Revew Form format, took away date and picture input boxes as hidden empty elements for future update, and add select box to review selections for user simplicity

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -3,12 +3,11 @@ import { updateReviewForm } from '../actions/reviewForm.js'
 import { connect } from 'react-redux'
 
 
-const ReviewForm = ({newReviewData, updateReviewForm, user_id, handleSubmit, editMode}) => {
 
 
-   
+const ReviewForm = ({newReviewData, updateReviewForm, user_id, history, rec_area_id, handleSubmit, editMode}) => {   
 
-   const handleChange = event => {
+const handleChange = event => {
       const { name, value } = event.target
       
       updateReviewForm(name, value)
@@ -16,73 +15,99 @@ const ReviewForm = ({newReviewData, updateReviewForm, user_id, handleSubmit, edi
 
 return ( 
 
- 
+    
+
 <form onSubmit={event => {
   event.preventDefault() 
-  
-  
-   
-   handleSubmit(newReviewData, user_id)}}>
 
+   handleSubmit(newReviewData, user_id, rec_area_id)}}>
 
+{/* 
    <input
    placeholder="rec_area_id"
+   type="hidden"
    name="rec_area_id"
    onChange={handleChange}
 
-value={newReviewData.rec_area_id} 
-/>
-
-<input
+value={rec_id} 
+/> */}
+<h3 className="review-form-question">How would you describe your experience overall?</h3>
+<p><textarea
 placeholder="description"
+rows="4" 
+cols="75"
 name="description"
 onChange={handleChange}
 value={newReviewData.description}
-/>
+/></p>
 
+{/* Leaving set up for future integration of pictures */}
 <input
 placeholder="picture"
+type="hidden"
 name="picture"
 onChange={handleChange}
-value={newReviewData.picture}
+value=" "
 />
 
-{/* Make a selector for month/year if possible. Can I do format like 01-2020 for date datatype? */}
+{/* Leaving set up for future integration of date  */}
 
 <input
 placeholder="date"
+type="hidden"
 name="date"
 onChange={handleChange}
-value={newReviewData.date}
+value= " "
 />
 
-<input
-placeholder="nature_review"
-name="nature_review"
-onChange={handleChange}
-value={newReviewData.nature_review}
-/>
+<h3 className="review-form-question">On a scale of 1-5, please rate the following:</h3>
+<h3></h3>
 
-<input
-placeholder="amenities_review"
-name="amenities_review"
-onChange={handleChange}
-value={newReviewData.amenities_review}
-/>
+<p className="review-form-sub-question">The beauty and/or distinctiveness of natural elements:</p>
 
-<input
-placeholder="activities_review"
-name="activities_review"
-onChange={handleChange}
-value={newReviewData.activities_review}
-/>
+<select name="nature_review" id="rating-select" onChange={handleChange} value={newReviewData.nature_review}>
+            <option value="">-- SELECT RATING --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        <h3></h3>
 
-<input
-placeholder="family_review"
-name="family_review"
-onChange={handleChange}
-value={newReviewData.family_review}
-/>
+<p className="review-form-sub-question">Quality of amenities provided (if applicable):</p>
+<select name="amenities_review" id="rating-select" onChange={handleChange} value={newReviewData.amenities_review}>
+            <option value="">-- SELECT RATING --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        <h3></h3>
+
+<p className="review-form-sub-question">Breadth and/or quality of activities available:</p>
+
+<select name="activities_review" id="rating-select" onChange={handleChange} value={newReviewData.activities_review}>
+            <option value="">-- SELECT RATING --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        <h3></h3>
+
+<p className="review-form-sub-question">Quality of activities and/or ease of access for families with small children:</p>
+<select name="family_review" id="rating-select" onChange={handleChange} value={newReviewData.family_review}>
+            <option value="">-- SELECT RATING --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        <h3></h3>
 
 <input type="submit" value={editMode ? "Update Review" : "Create Review"}/>
 
@@ -103,7 +128,7 @@ const mapStateToProps = state => {
    return {
       newReviewData: state.reviewForm,
       myReviews: state.myReviews,
-      user_id
+      user_id,
 }
 }
 
