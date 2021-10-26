948ae92df13b74f919bf0f0601ea76f7437c1dea
mrandolph83 <mrandolph83@gmail.com>
2021-10-25 23:38:03 -0400

    Update MyReview display data, to rec area name, added section header text

--- src/components/MyReviews.js
+++ src/components/MyReviews.js
@@ -2,11 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-const MyReviews = props => {
-    const reviewCards = props.reviews.length > 0 ?
-    props.reviews.map(review => (<p key={review.id}><Link to={`/reviews/${review.id}`}> {review.attributes.description} </Link> </p>)) : null
-return reviewCards 
 
+
+
+const MyReviews = props => {
+   
+    const reviewCards = props.reviews.length > 0 ?
+    props.reviews.map(review => (<p key={review.id}><Link to={`/reviews/${review.id}`}> {review.attributes.rec_area.name} </Link> ({review.attributes.rec_area.state} )</p>)) : null
+
+return (
+
+<>   
+<div>
+    <h3>Edit or Delete your Reviews Below:</h3>
+    </div> 
+{reviewCards} 
+</>
+)
 }
 
 const mapStateToProps = state => {
